7494ea4fe3685147d3307d15d52d40b9743de32b
saikat <saikat2990@gmail.com>
2023-03-04 01:25:34 +0600

    compress add

---
+++ LeetCrackToLifeGoal/Compress.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCrackToLifeGoal
+{
+    public class Compress
+    {
+        public static int Compresss(char[] chars)
+        {
+            int i = 0;
+            int j = 0;
+            string str = "";
+            while (chars.Length > i)
+            {
+                var ch = chars[i];
+                j = i;
+                var count = 0;
+                while (chars.Length > j && chars[j] == ch)
+                {
+                    j++;
+                    count++;
+                }
+
+                str += ch.ToString();
+
+                if (count > 1)
+                {
+                    var countString = count.ToString();
+                    foreach (var cS in countString)
+                    {
+                        i++;
+                        str += cS.ToString();
+                    }
+                }
+
+                i = j;
+
+            }
+
+
+            for (int k = 0; k < str.Length; k++)
+            {
+                chars[k] = str[k];
+            }
+            return str.Length;
+        }
+
+
+    }
+}
--- LeetCrackToLifeGoal/Program.cs
+++ LeetCrackToLifeGoal/Program.cs
@@ -6,41 +6,13 @@ using System.Xml.Linq;
 
 namespace leetCrack
 {
-    public class Node
-    {
-        public int val;
-        public IList<Node> neighbors;
-
-        public Node()
-        {
-            val = 0;
-            neighbors = new List<Node>();
-        }
-
-        public Node(int _val)
-        {
-            val = _val;
-            neighbors = new List<Node>();
-        }
-
-        public Node(int _val, List<Node> _neighbors)
-        {
-            val = _val;
-            neighbors = _neighbors;
-        }
-    }
-
-    public class characterData{
-        public int totalOccur { get; set; }
-        public int serial { get; set; }
-        public char ch { get; set; }
-    }
+    
 
     public class Program
     {
-       public static void Main(string[] args)
+      public static void Main(string[] args)
         {
-            Console.WriteLine();
+            
         }
 
 
