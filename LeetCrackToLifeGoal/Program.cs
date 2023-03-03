c47017e8b853efd7e5d0b11fb169456533a0cbeb
saikat <saikat2990@gmail.com>
2023-03-03 23:44:12 +0600

    add previous code

--- LeetCrackToLifeGoal/Program.cs
+++ LeetCrackToLifeGoal/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace LeetCrackToLifeGoal
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace leetCrack
 {
-    internal class Program
+    public class Node
     {
-        static void Main(string[] args)
+        public int val;
+        public IList<Node> neighbors;
+
+        public Node()
         {
-            Console.WriteLine("Hello, World!");
+            val = 0;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val, List<Node> _neighbors)
+        {
+            val = _val;
+            neighbors = _neighbors;
         }
     }
+
+    public class characterData{
+        public int totalOccur { get; set; }
+        public int serial { get; set; }
+        public char ch { get; set; }
+    }
+
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+
+
+    }
+   
 }
