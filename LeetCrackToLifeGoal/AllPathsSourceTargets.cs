ce892a43fb023410b76cc7bbf5a17b7b799ff3b1
saikat <saikat2990@gmail.com>
2023-03-05 02:10:28 +0600

    AllPathsSourceTarget solved

---
+++ LeetCrackToLifeGoal/AllPathsSourceTargets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCrackToLifeGoal
+{
+    public class AllPathsSourceTargets
+    {
+        public static void checkAnddAdd(int[][] graph, IList<IList<int>> final, IList<int> current, int index, int finish)
+        {
+
+            if (graph[index].Length == 0 && finish == index)
+            {
+                IList<int> newList = new List<int>();
+                foreach (var val in current)
+                {
+                    newList.Add(val);
+                }
+                final.Add(newList);
+            }
+            else
+            {
+                foreach (var data in graph[index])
+                {
+                    current.Add(data);
+                    checkAnddAdd(graph, final, current, data, finish);
+                    current.RemoveAt(current.Count - 1);
+                }
+            }
+
+        }
+
+        public static IList<IList<int>> AllPathsSourceTarget(int[][] graph)
+        {
+            var finalAnswer = new List<IList<int>>();
+            var finishPoint = graph.Length - 1;
+            foreach (var node in graph[0])
+            {
+                var current = new List<int>();
+                current.Add(0);
+                current.Add(node);
+                checkAnddAdd(graph, finalAnswer, current, node, finishPoint);
+            }
+            return finalAnswer;
+        }
+    }
+}
--- LeetCrackToLifeGoal/Program.cs
+++ LeetCrackToLifeGoal/Program.cs
@@ -11,46 +11,10 @@ namespace leetCrack
     public class Program
     {
         
-        public static void checkAnddAdd(int[][] graph, IList<IList<int>> final, IList<int> current,int index,int finish)
-        {
-           
-            if (graph[index].Length == 0 && finish==index)
-            {
-               IList<int> newList= new List<int>();
-               foreach (var val in current)
-               {
-                    newList.Add(val);
-               }
-                final.Add(newList);
-            }
-            else
-            {
-                foreach (var data in graph[index])
-                {
-                    current.Add(data);
-                    checkAnddAdd(graph, final, current, data, finish);
-                    current.RemoveAt(current.Count-1);
-                }
-            }
-
-        }
-
-        public static IList<IList<int>> AllPathsSourceTarget(int[][] graph)
-        {
-            var finalAnswer = new List<IList<int>>();
-            var finishPoint = graph.Length - 1;
-            foreach (var node in graph[0])
-            {
-                var current = new List<int>();
-                current.Add(0);
-                current.Add(node);
-                checkAnddAdd(graph, finalAnswer, current, node, finishPoint);
-            }
-            return finalAnswer;
-        }
+       
         public static void Main(string[] args)
         {
-            AllPathsSourceTarget(new int[][] { new int[] { 4, 3, 1 }, new int[] { 3, 2, 4 }, new int[] {  }, new int[]{4} , new int[]{}});
+            //AllPathsSourceTarget(new int[][] { new int[] { 4, 3, 1 }, new int[] { 3, 2, 4 }, new int[] {  }, new int[]{4} , new int[]{}});
         }
 
 
