eb91b318aad77fe2074b1b974360e2c53203f685
saikat <saikat2990@gmail.com>
2023-03-04 18:41:20 +0600

    MoveZeros complete

--- LeetCrackToLifeGoal/Program.cs
+++ LeetCrackToLifeGoal/Program.cs
@@ -10,13 +10,40 @@ namespace leetCrack
 
     public class Program
     {
-        public void MoveZeroes(int[] nums)
+        public static void MoveZeroes(int[] nums)
         {
+            var countZero = 0;
+            var notZeros = new List<int>();
+            foreach (var num in nums)
+            {
+                if (num != 0)
+                {
+                    notZeros.Add(num);
+                }
+                else
+                {
+                    countZero++;
+                }
+            }
 
+            for (int i = 0; i < notZeros.Count; i++)
+            {
+                nums[i] = notZeros[i];
+            }
+
+            for (int i = notZeros.Count; i <notZeros.Count+countZero; i++)
+            {
+                nums[i] = 0;
+            }
+
+            foreach (var VARIABLE in nums)
+            {
+                Console.Write(VARIABLE);
+            }
         }
         public static void Main(string[] args)
         {
-            
+            MoveZeroes(new int[] { 1, 2, 3, 0, 6, 0, 9, 3 });
         }
 
 
