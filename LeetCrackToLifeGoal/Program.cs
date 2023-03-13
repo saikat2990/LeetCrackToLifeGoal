c9eb2740eadcf9c3e79f301d8ac370e4880fd48f
saikat <saikat2990@gmail.com>
2023-03-14 03:10:17 +0600

    mergetwoList

--- LeetCrackToLifeGoal/Program.cs
+++ LeetCrackToLifeGoal/Program.cs
@@ -3,73 +3,16 @@ using System.Collections;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
+using static LeetCrackToLifeGoal.MergeKListss;
 
 namespace leetCrack
 {
 
     public class Program
-    { 
-        public class TreeNode
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
-             public int val;
-             public TreeNode left;
-             public TreeNode right;
-             public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-             {
-                this.val = val;
-                this.left = left;
-                this.right = right;
-             }
-        }
 
-        public void traverseRight(TreeNode node, List<TreeNode> rightAnswer)
-        {
-           
-            if (node != null)
-            {
-                rightAnswer.Add(node);
-                traverseRight(node.right, rightAnswer);
-                traverseRight(node.left, rightAnswer);
-            }
-            else
-            {
-                rightAnswer.Add(null);
-            }
-        }
-        public void traverseLeft(TreeNode node,List<TreeNode> leftAnswer)
-        {
-            if (node != null)
-            {
-                leftAnswer.Add(node);
-                traverseRight(node.left, leftAnswer);
-                traverseRight(node.right, leftAnswer);
-            }
-            else
-            {
-                leftAnswer.Add(null);
-            }
-
-        }
-        public bool IsSymmetric(TreeNode root)
-        {
-            var leftAnswer = new List<TreeNode>();
-            var rightAnswer = new List<TreeNode>();
-            traverseLeft(root.left, leftAnswer);
-            traverseRight(root.right, rightAnswer);
-            for (int i = 0; i < leftAnswer.Count; i++)
-            {
-                if(leftAnswer[i]==null && rightAnswer[i]==null)
-                    continue;
-                if (leftAnswer[i] != null && rightAnswer[i] == null)
-                    return false;
-                if (leftAnswer[i] == null && rightAnswer[i] != null)
-                    return false;
-                Console.WriteLine(leftAnswer[i].val);
-                Console.WriteLine(rightAnswer[i].val);
-
-                if (leftAnswer[i].val != rightAnswer[i].val) return false;
-            }
-            return true;
         }
         public static void Main(string[] args)
         {
