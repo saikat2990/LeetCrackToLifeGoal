5767bc6d6e44ec7fe9eb36e251f263f20d5b2eff
saikat <saikat2990@gmail.com>
2023-03-17 17:33:30 +0600

    Remove_Nth_Node

---
+++ LeetCrackToLifeGoal/RemoveNthFromEnds.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCrackToLifeGoal.MergeKListss;
+
+namespace LeetCrackToLifeGoal
+{
+    internal class RemoveNthFromEnds
+    {
+        public ListNode ReverseNode(ListNode head)
+        {
+            ListNode rest = null;
+            while (head != null)
+            {
+                rest = new ListNode(head.val, rest);
+                head = head.next;
+            }
+
+            return rest;
+        }
+
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+
+            var reverseNode = ReverseNode(head);
+            if (n == 1) return ReverseNode(reverseNode.next);
+            var it = 0;
+            head = reverseNode;
+            ListNode Head = head;
+            ListNode previousNode = head;
+            while (it < n)
+            {
+                previousNode = head;
+                head = head.next;
+                it++;
+            }
+            previousNode.next = head.next;
+
+            return ReverseNode(Head);
+        }
+    }
+}
