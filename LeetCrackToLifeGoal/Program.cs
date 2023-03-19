b245231cb908d3f572a69504565e4b734f972d65
saikat <saikat2990@gmail.com>
2023-03-19 21:38:01 +0600

    buildTreePreIn_PostIn_to_binaryTree

--- LeetCrackToLifeGoal/Program.cs
+++ LeetCrackToLifeGoal/Program.cs
@@ -3,24 +3,7 @@
 
     public class Program
     {
-        public TreeNode BuildTree(int[] preorder, int[] inorder)
-        {
-            if (inorder.Length == 0 || preorder.Length == 0)
-            {
-                return null;
-            }
-
-            var root = preorder[0];
-            var node = new TreeNode(root);
-            var mid = Array.IndexOf(inorder, root);
-            var leftInorder = inorder.Skip(0).Take(mid).ToArray();
-            var rightInoder = inorder.Skip(mid+1).Take(inorder.Length - mid - 1).ToArray();
-            var leftPostorder = preorder.Skip(1).Take(mid+1).ToArray();
-            var rightPostorder = preorder.Skip(mid+1).Take(inorder.Length - mid - 1).ToArray();
-            node.left = BuildTree(leftPostorder,leftInorder );
-            node.right = BuildTree(rightPostorder,rightInoder);
-            return node;
-        }
+       
 
         public static void Main(string[] args)
         {
