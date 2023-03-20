325ac6fb963efa2f4fd3fad43daf971ab2fc1c3f
saikat <saikat2990@gmail.com>
2023-03-21 02:50:10 +0600

    CanPLugged

---
+++ LeetCrackToLifeGoal/CanPlaceFlowerss.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCrackToLifeGoal
+{
+    internal class CanPlaceFlowerss
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            int pluged = 0;
+            if (flowerbed.Length == 1 && flowerbed[0] == 0)
+            {
+                flowerbed[0] = 1;
+                pluged++;
+            }
+
+
+
+            if (flowerbed.Length >= 2 && flowerbed[flowerbed.Length - 1] == 0 && flowerbed[flowerbed.Length - 2] == 0)
+            {
+                flowerbed[flowerbed.Length - 1] = 1;
+                flowerbed[flowerbed.Length - 2] = -1;
+                pluged++;
+            }
+            if (flowerbed.Length >= 2 && flowerbed[0] == 0 && (flowerbed[1] == 0 || flowerbed[1] == -1))
+            {
+                flowerbed[0] = 1;
+                flowerbed[1] = -1;
+                pluged++;
+            }
+
+            if (flowerbed.Length > 2)
+            {
+                for (int i = 2; i < flowerbed.Length - 1; i++)
+                {
+                    if (flowerbed[i] == 0 && (flowerbed[i - 1] == 0 || flowerbed[i - 1] == -1) &&
+                        ((flowerbed[i + 1] == 0 || flowerbed[i + 1] == -1)))
+                    {
+                        flowerbed[i] = 1;
+                        flowerbed[i - 1] = -1;
+                        flowerbed[i + 1] = -1;
+                        pluged++;
+                    }
+                }
+            }
+
+            if (pluged >= n) return true;
+            return false;
+        }
+
+    }
+}
---
+++ LeetCrackToLifeGoal/MaxDepthss.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCrackToLifeGoal
+{
+   
+    public class MaxDepthss
+    {
+        public class Node
+        {
+            public int val;
+            public IList<Node> children;
+
+            public Node() { }
+
+            public Node(int _val)
+            {
+                val = _val;
+            }
+
+            public Node(int _val, IList<Node> _children)
+            {
+                val = _val;
+                children = _children;
+            }
+        }
+        public int MaxDepth(Node root)
+        {
+            var queue = new Queue<Node>();
+            var check = 0;
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                check++;
+                var nodeList = new List<Node>();
+                while (queue.Count > 0)
+                {
+                    nodeList.Add(queue.Dequeue());
+                }
+
+                foreach (var node in nodeList)
+                {
+                    foreach (var nodeCh in node.children)
+                    {
+                        queue.Enqueue(nodeCh);
+                    }
+                }
+            }
+
+            return check;
+        }
+    }
+}
