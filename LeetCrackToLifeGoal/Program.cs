fa3f1113753a2df9223ac4e91d900bfc0edc0a7e
saikat <saikat2990@gmail.com>
2023-03-22 03:28:55 +0600

    zeros

--- LeetCrackToLifeGoal/Program.cs
+++ LeetCrackToLifeGoal/Program.cs
@@ -6,10 +6,62 @@ namespace leetCrack
     public class Program
     {
 
-       
+        public static TreeNode CreateNewNode(TreeNode root)
+        {
+            var newRoot = new TreeNode(0);
+            if (root.right != null)
+            {
+                newRoot.right = CreateNewNode(root.right);
+            }
+           
+
+            if (root.left != null)
+            {
+                newRoot.left = CreateNewNode(root.left);
+            }
+
+            return newRoot;
+        }
+
+        public static void construct(TreeNode root, TreeNode node, int current, int n, List<TreeNode> ans)
+        {
+            if (n == 1) root = node;
+            if (current + 2 == n)
+            {
+                node.right = new TreeNode(0);
+                node.left = new TreeNode(0);
+                var data = CreateNewNode(root);
+                ans.Add(data);
+                node.right = null;
+                node.left = null;
+            }
+            else if (current < n)
+            {
+
+                node.right = new TreeNode(0);
+                node.left = new TreeNode(0);
+                current += 1;
+                construct(root, node.right, current, n, ans);
+               
+                current += 1;
+                
+                construct(root, node.left, current, n, ans);
+                node.right = null;
+                node.left = null;
+            }
+        }
+        public static IList<TreeNode> AllPossibleFBT(int n)
+        {
+            var answerList = new List<TreeNode>();
+            var root = new TreeNode(0);
+            var node = new TreeNode(0);
+            construct(root, root, 1, n, answerList);
+            return answerList;
+        }
+
         public static void Main(string[] args)
         {
-           
+            AllPossibleFBT(7);
         }
 
 
---
+++ LeetCrackToLifeGoal/ZeroFilledSubarrays.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCrackToLifeGoal
+{
+    internal class ZeroFilledSubarrays
+    {
+        public static long ZeroFilledSubarray(int[] nums)
+        {
+            long active = 0;
+            var numberOfOccur = new Dictionary<int, int>();
+            var num = 0;
+            long numsOfZeros = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    num++;
+
+                }
+                else
+                {
+                    for (int j = 1; j <= num; j++)
+                    {
+                        var data = (num - j + 1);
+                        if (numberOfOccur.ContainsKey(j))
+                        {
+                            numberOfOccur[j] += data;
+                        }
+                        else
+                        {
+                            numberOfOccur.Add(j, data);
+                        }
+                    }
+                    num = 0;
+                }
+            }
+            for (int j = 1; j <= num; j++)
+            {
+                var data = (num - j + 1);
+                if (numberOfOccur.ContainsKey(j))
+                {
+                    numberOfOccur[j] += data;
+                }
+                else
+                {
+                    numberOfOccur.Add(j, data);
+                }
+            }
+
+            foreach (var val in numberOfOccur.Values)
+            {
+                numsOfZeros += val;
+            }
+
+            return numsOfZeros;
+        }
+
+    }
+}
