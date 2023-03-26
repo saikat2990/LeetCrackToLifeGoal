a92160656ed1a7cd4f98f61409fcae8390e9b92b
saikat <saikat2990@gmail.com>
2023-03-27 01:19:41 +0600

    subarraysum

--- LeetCrackToLifeGoal/Program.cs
+++ LeetCrackToLifeGoal/Program.cs
@@ -9,91 +9,44 @@ namespace leetCrack
     public class Program
     {
 
-        public int FindMin(int[] nums)
+        public static int MinSubArrayLen(int target, int[] nums)
         {
-            if (nums == null || nums.Length == 0) return 0;
+            var right = 0;
             var left = 0;
-            var right = nums.Length - 1;
-            var mid = 0;
-            while (left<right)
+            var sum = 0;
+            var count = nums.Length+1;
+            var store = (nums[0]);
+            while (true)
             {
-                mid = left + (right - left) / 2;
-                if (nums[mid] < nums[right])
+                if (target > store)
                 {
-                    right = mid;
+                    if (right >= nums.Length - 1) break;
+                    right++;
+                    store+=(nums[right]);
                 }
                 else
                 {
-                    left = mid+1;
-                }
-            }
-            return nums[left];
-        }
-        public int FindPeakElement(int[] nums)
-        {
-            if (nums.Length == 1) return 0;
-            if (nums[0] > nums[1]) return 0;
-            if (nums[nums.Length - 1] > nums[nums.Length-2])return nums.Length-1;
-
-            for (int i = 1; i < nums.Length-1; i++)
-            {
-                if (nums[i - 1] < nums[i] && nums[i + 1] < nums[i]) 
-                    return i;
-            }
-
-            return -1;
-        }
-        public ListNode DeleteDuplicates(ListNode head)
-        {
-            if (head == null) return head;
-            var listElements = new List<int>();
-            var prev = head;
-            var next = head.next;
-
-            while (next != null)
-            {
-
-                if (prev.val == next.val)
-                {
-                    listElements.Add(next.val);
-                }
-                prev = next;
-                next = next.next;
-            }
-
-            prev = head;
-            next = head.next;
-            while (prev != null && listElements.Contains(prev.val))
-            {
-                prev = prev.next;
-            }
-            head = prev;
-            if (prev != null)
-            {
-                next = prev.next;
-                while (next != null)
-                {
-                    if (listElements.Contains(next.val))
+                    if ((right - left + 1) < count)
                     {
-                        while (next != null && listElements.Contains(next.val))
-                        {
-                            next = next.next;
-                        }
+                        count = right - left + 1;
+                        if (count == 1) return count;
 
-                        prev.next = next;
                     }
-                    prev = next;
-                    if (next != null) next = next.next;
+                   
+                    store-=nums[left];
+                    left++;
                 }
 
             }
 
-            return head;
+            if (count > nums.Length) return 0;
+            return count;
         }
+
         public static void Main(string[] args)
         {
 
-
+            MinSubArrayLen(7, new int[] { 2, 3, 1, 2, 4, 3 });
         }
 
        
