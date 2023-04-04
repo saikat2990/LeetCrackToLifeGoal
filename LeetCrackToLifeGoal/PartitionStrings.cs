b020b286e2966cc445208f1c04e3a5b8440d7326
saikat <saikat2990@gmail.com>
2023-04-04 08:32:16 +0600

    partisionString

---
+++ LeetCrackToLifeGoal/PartitionStrings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCrackToLifeGoal
+{
+    internal class PartitionStrings
+    {
+        public static int PartitionString(string s)
+        {
+            var length = s.Length;
+            List<string> ans = new List<string>();
+            for (int i = 0; i < length; i++)
+            {
+                var charTag = new bool[26];
+                var str = "";
+                var index = i;
+                for (int j = i; j < length; j++)
+                {
+                    if (charTag[s[j] - 'a'] == true)
+                    {
+                        i = j - 1;
+                        ans.Add(str);
+                        str = "";
+                        break;
+                    }
+                    charTag[s[j] - 'a'] = true;
+                    str += s[j].ToString();
+                    index = j;
+
+                }
+                if (index != i) i = index;
+                if (str != "")
+                    ans.Add(str);
+            }
+            return ans.Count;
+        }
+    }
+}
--- LeetCrackToLifeGoal/Program.cs
+++ LeetCrackToLifeGoal/Program.cs
@@ -7,11 +7,11 @@ namespace leetCrack
 
     public class Program
     {
-
+       
 
         public static void Main(string[] args)
         {
-       
+            
         }
 
        
